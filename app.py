4166a25adc521c80df0665d96348c0f4442bd613
deffy316 <phoompanna@gmail.com>
2025-05-21 19:51:39 +0700

    add flask app and UI

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, render_template, jsonify
+from spell_model import SpellChecker
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template("gui.html",result=None)
+
+@app.route('/correct', methods=['POST'])
+def correct():
+    input_text = request.form.get('input_text').strip().lower()
+    
+    # Process the input_text (example: reversing the text)
+    if not input_text:
+        # TODO: Warn User
+        return
+
+    spc = SpellChecker()
+    suggestion = spc.suggest(word=input_text)
+    result = f"Suggestion:{suggestion}"
+    return jsonify({'result': result})
+
+if __name__ == '__main__':
+    app.run(debug=True)
